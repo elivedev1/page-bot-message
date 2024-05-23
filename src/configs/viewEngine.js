b9691661dc8766b3653f080abdccc3853f9b2d74
elive dev1 <elive.dev1@gmail.com>
2024-05-23 17:28:19 +0700

    KhaD

--- src/configs/viewEngine.js
+++ src/configs/viewEngine.js
@@ -1,11 +1,33 @@
 import express from "express";
+import path from "path";
+import { __dirname } from "../utils.js";
 
 let configViewEngine = (app) => {
-  app.use(express.static("./src/public"));
+  const publicPath = path.join(__dirname, "../src/public");
+  const viewsPath = path.join(__dirname, "../src/views");
+
+  console.log("Public path: ", publicPath);
+  console.log("Views path: ", viewsPath);
+
+  app.use(express.static(publicPath));
 
   app.set("view engine", "ejs");
 
-  app.set("views", "./src/views");
+  app.set("views", viewsPath);
 };
 
 export default configViewEngine;
+
+//////////////////////////////////////////////////////////////////////
+
+// import express from "express";
+
+// let configViewEngine = (app) => {
+//   app.use(express.static("./src/public"));
+
+//   app.set("view engine", "ejs");
+
+//   app.set("views", "./src/views");
+// };
+
+// export default configViewEngine;
