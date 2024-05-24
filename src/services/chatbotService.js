427c790c63e5c80b640d6c652c425e484ca7bf57
elive dev1 <elive.dev1@gmail.com>
2024-05-24 08:03:43 +0700

    KHaD

--- src/services/chatbotService.js
+++ src/services/chatbotService.js
@@ -361,11 +361,9 @@ const getMainMenuTemplate = () => {
             image_url: IMAGE_MAIN_MENU_2,
             buttons: [
               {
-                type: "web_url",
-                url: `${process.env.URL_WEB_VIEW_ORDER}`,
+                type: "postback",
                 title: "ĐẶT BÀN",
-                webview_height_tatio: "tall",
-                messenger_extensions: true,
+                payload: "RESERVE_TABLE",
               },
             ],
           },
